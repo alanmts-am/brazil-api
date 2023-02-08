c684d61ee7b68e744373c0ebc587b454560650ef
alanmts-am <alanmts.am@gmail.com>
2023-02-07 21:51:34 -0300

    refactor: remove unused files

--- app/models/database.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import os
-
-class Database:
-    def __init__(self) -> None:
-        pass
-    
-    def update(self, url:str, output:str):
-        subprocess.run(["wget", url, "-O", output])
