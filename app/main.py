fa5b7b4a57e8c1c84becf114349e5565f1cb1464
alanmts-am <alanmts.am@gmail.com>
2023-10-04 21:39:39 -0300

    feat: implements estados endpoints

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from app.models.paises import Paises
+from app.models.estados import Estados
 
 from fastapi import FastAPI
 
 app = FastAPI()
 paises = Paises()
+estados = Estados()
 
 @app.get('/paises')
 async def get_paises():
@@ -16,3 +18,15 @@ async def get_paises_nomes():
 @app.get('/paises/{id}')
 async def get_paises_id(id:int):
     return paises.getById(id)
+
+@app.get('/estados')
+async def get_estados():
+    return estados.getAll()
+
+@app.get('/estados/nomes')
+async def geta_estados_name_only():
+    return estados.getNamesOnly()
+
+@app.get('/estados/{id}')
+async def get_estados_by_id(id:int):
+    return estados.getEstadoById(id)
---
+++ app/models/estados.py
@@ -0,0 +1,36 @@
+import json as j
+
+class Estados:
+    def __init__(self) -> None:
+        with open('./app/archives/estados.json', 'rb') as f:
+            self.json = j.load(f)
+    
+    def getAll(self):
+        estados = []
+
+        for e in self.json:
+            estado = {}
+            estado['id'] = e['id']
+            estado['sigla'] = e['sigla']
+            estado['nome'] = e['nome']
+            estado['regiao'] = e['regiao']['nome']
+
+            estados.append(estado)
+
+        return estados
+    
+    def getEstadoById(self, id:int):
+        for e in self.getAll():
+            if e['id'] == id:
+                return e
+            
+    def getNamesOnly(self):
+        estados = []
+
+        for e in self.getAll():
+            estado = {}
+            estado['nome'] = e['nome']
+
+            estados.append(estado)
+
+        return estados
