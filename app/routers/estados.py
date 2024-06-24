fa273d62c9ff30307c1346ae475dabd1b190ae54
alanmts-am <alanmts.am@gmail.com>
2024-06-24 16:22:48 -0300

    feat: add /municipios to estados endpoint

--- app/routers/estados.py
+++ app/routers/estados.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from app.models.estados import Estados
+from app.models.municipios import Municipio
 
 
 estados_router = APIRouter()
 estados = Estados()
+municipios = Municipio()
 
 
 @estados_router.get('/')
@@ -19,3 +21,8 @@ async def geta_estados_name_only():
 @estados_router.get('/{id}')
 async def get_estados_by_id(id: int):
     return estados.get_by_id(id)
+
+
+@estados_router.get('/{id}/municipios')
+async def get_municipios_by_estado(id: int):
+    return estados.get_municipios_from_estado(municipios.get_all(), id)
