f0d01f5e8d1d14906e14cde606f08e8a258f572a
alanmts-am <alanmts.am@gmail.com>
2024-06-24 17:35:11 -0300

    refactor: change estados endpoint JSON return

--- app/models/estados.py
+++ app/models/estados.py
@@ -8,7 +8,9 @@ class Estado:
             self.json = j.load(f)
 
     def get_all(self):
+        estados_dict = {}
         estados = []
+        estados_dict['status'] = 'OK'
 
         for e in self.json:
             estado = {}
@@ -19,29 +21,41 @@ class Estado:
 
             estados.append(estado)
 
-        return estados
+        estados_dict['estados'] = estados
+        return estados_dict
 
     def get_by_id(self, id: int):
-        for e in self.get_all():
+        estado_dict = {}
+        estado = []
+        estado_dict['status'] = 'OK'
+
+        for e in self.get_all()['estados']:
             if e['id'] == id:
-                return e
+                estado.append(e)
+
+        estado_dict['estado'] = estado
+        return estado_dict
 
     def get_names_only(self):
-        estados = []
+        estados_nome = []
+        estados = {}
+        estados['status'] = 'OK'
 
         for e in self.get_all():
-            estado = {}
-            estado['nome'] = e['nome']
+            estados_nome.append(e['nome'])
 
-            estados.append(estado)
+        estados['nomes'] = sorted(estados_nome)
 
-        return sorted(estados, key=itemgetter("nome"))
+        return estados
 
     def get_municipios_from_estado(self, municipios: list, id_estado: int):
+        municipios_estado_dict = {}
         municipios_estado = []
+        municipios_estado_dict['status'] = 'OK'
 
         for municipio in municipios:
             if municipio['estado']['id'] == id_estado:
                 municipios_estado.append(municipio)
+        municipios_estado_dict['municipios'] = municipios_estado
 
-        return municipios_estado
+        return municipios_estado_dict
