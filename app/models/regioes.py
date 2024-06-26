0e1aa5124565d688480e39780f2c9cd95a652510
alanmts-am <alanmts.am@gmail.com>
2024-06-26 19:38:57 -0300

    refactor: update regioes logic to accept Response class

--- app/models/regioes.py
+++ app/models/regioes.py
@@ -37,10 +37,27 @@ class Regiao:
 
         return sorted(regioes, key=itemgetter("nome"))
 
-    def get_estados_from_regiao(self, estados: list, id_regiao: int):
+    def get_estados(self, estados: list, id_regiao: int):
         estados_regiao = []
         for estado in estados:
             if estado['regiao']['id'] == id_regiao:
                 estados_regiao.append(estado)
 
         return estados_regiao
+
+    def get_estado(self, estados: list, id_regiao: int, id_estado: int):
+        estados_regiao = self.get_estados(
+            estados, id_regiao)
+
+        for estado in estados_regiao:
+            if estado['id'] == id_estado:
+                return estado
+
+    def get_municipio(self, municipios: list, id_regiao: int, id_estado: int, id_municipio: int):
+        municipios_estado = []
+
+        for municipio in municipios:
+            if municipio['id'] == id_municipio and municipio['estado']['id'] == id_estado and municipio['estado']['regiao']['id'] == id_regiao:
+                municipios_estado.append(municipio)
+
+        return municipios_estado
--- app/routers/regioes.py
+++ app/routers/regioes.py
@@ -9,25 +9,6 @@ estados = Estado()
 municipios = Municipio()
 
 
-def get_estado(estados: list, id_regiao: int, id_estado: int):
-    estados_regiao = regioes.get_estados_from_regiao(
-        estados, id_regiao)
-
-    for estado in estados_regiao:
-        if estado['id'] == id_estado:
-            return estado
-
-
-def get_municipio(municipios: list, id_regiao: int, id_estado: int, id_municipio: int):
-    municipios_estado = []
-
-    for municipio in municipios:
-        if municipio['id'] == id_municipio and municipio['estado']['id'] == id_estado and municipio['estado']['regiao']['id'] == id_regiao:
-            municipios_estado.append(municipio)
-
-    return municipios_estado
-
-
 @regioes_router.get('/')
 async def get_regioes():
     return regioes.get_all()
@@ -45,21 +26,22 @@ async def get_regioes_by_id(id: int):
 
 @regioes_router.get('/{id}/estados')
 async def get_estados_from_regiao(id: int):
-    return regioes.get_estados_from_regiao(estados.get_all(), id)
+    return regioes.get_estados(estados.get_all()['estados'], id)
 
 
 @regioes_router.get('/{id_regiao}/estados/{id_estado}')
 async def get_estado_from_regiao(id_regiao: int, id_estado: int):
-    return get_estado(estados.get_all(), id_regiao, id_estado)
+    return regioes.get_estado(estados.get_all()['estados'], id_regiao, id_estado)
 
 
 @regioes_router.get('/{id_regiao}/estados/{id_estado}/municipios')
 async def get_municipios_from_estado(id_regiao: int, id_estado: int):
-    estado = get_estado(estados.get_all(), id_regiao, id_estado)
+    estado = regioes.get_estado(
+        estados.get_all()['estados'], id_regiao, id_estado)
 
-    return estados.get_municipios_from_estado(municipios.get_all(), estado['id'])
+    return estados.get_municipios_from_estado(municipios.get_all()['municipios'], estado['id'])
 
 
 @regioes_router.get('/{id_regiao}/estados/{id_estado}/municipios/{id_municipio}')
 async def get_municipio_from_estado(id_regiao: int, id_estado: int, id_municipio: int):
-    return get_municipio(municipios.get_all(), id_regiao, id_estado, id_municipio)
+    return regioes.get_municipio(municipios.get_all()['municipios'], id_regiao, id_estado, id_municipio)
