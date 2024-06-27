2e9939f33cc4302adecabfc44be3f1bcaf19ec41
alanmts-am <alanmts.am@gmail.com>
2024-06-26 22:07:18 -0300

    refactor: implements service logic in Pais class

--- app/models/paises.py
+++ app/models/paises.py
@@ -1,38 +1,8 @@
-import json as j
-from app.models.response import Response
-
-
 class Pais:
-    def __init__(self, response: Response) -> None:
-        self.response = response
-        with open('./app/archives/paises.json', 'rb') as f:
-            self.json = j.load(f)
-
-    def get_all(self):
-        paises = []
-
-        for p in self.json:
-            pais = {}
-            pais['id'] = p['id']['M49']
-            siglas = []
-            siglas.append(p['id']['ISO-ALPHA-2'])
-            siglas.append(p['id']['ISO-ALPHA-3'])
-            pais['siglas'] = siglas
-            pais['nome'] = p['nome']
-            pais['regiao'] = p['sub-regiao']['regiao']['nome']
-            pais['sub-regiao'] = p['sub-regiao']['nome']
-            paises.append(pais)
-
-        return self.response.sucess('paises', paises)
-
-    def get_by_id(self, id: int):
-        for p in self.get_all()['paises']:
-            if p['id'] == id:
-                return self.response.sucess('pais', [p])
-
-    def get_names_only(self):
-        nomes = []
-        for p in self.get_all()['paises']:
-            nomes.append(p['nome'])
-
-        return self.response.sucess('nomes', sorted(nomes))
+    def __init__(self, id, siglas: list, nome, regiao, sub_regiao) -> None:
+        self.id = id
+        self.siglas = siglas
+        self.nome = nome
+        self.regiao = regiao
+        self.sub_regiao = sub_regiao
+        pass
--- app/routers/paises.py
+++ app/routers/paises.py
@@ -1,22 +1,22 @@
 from fastapi import APIRouter
-from app.models.paises import Pais
+from app.service.pais_service import PaisService
 from app.models.response import Response
 
 paises_router = APIRouter()
 response = Response()
-paises = Pais(response)
+pais_service = PaisService(response)
 
 
 @paises_router.get('/')
 async def get_paises():
-    return paises.get_all()
+    return pais_service.get_all()
 
 
 @paises_router.get('/nomes')
 async def get_paises_nomes():
-    return paises.get_names_only()
+    return pais_service.get_names_only()
 
 
 @paises_router.get('/{id}')
 async def get_paises_id(id: int):
-    return paises.get_by_id(id)
+    return pais_service.get_by_id(id)
