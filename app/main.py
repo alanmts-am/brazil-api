f7de0ca5ca2ba6a8df8c1187458bedea219015e6
alanmts-am <alanmts.am@gmail.com>
2024-06-27 19:59:06 -0300

    refactor: update routers name

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from app.routers.paises import paises_router
-from app.routers.estados import estados_router
-from app.routers.regioes import regioes_router
-from app.routers.municipios import municipios_router
+from app.routers.estados import estado_router
+from app.routers.regioes import regiao_router
+from app.routers.municipios import municipio_router
 
 from fastapi import FastAPI
 
@@ -9,7 +9,7 @@ app = FastAPI()
 app.mount(path="/api", app=app)
 
 app.include_router(paises_router, prefix='/paises', tags=['paises'])
-app.include_router(estados_router, prefix='/estados', tags=['estados'])
-app.include_router(regioes_router, prefix='/regioes', tags=['regioes'])
-app.include_router(municipios_router, prefix='/municipios',
+app.include_router(estado_router, prefix='/estados', tags=['estados'])
+app.include_router(regiao_router, prefix='/regioes', tags=['regioes'])
+app.include_router(municipio_router, prefix='/municipios',
                    tags=['municipios'])
