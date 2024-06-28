990c74599fd711b5c0c94a37852027aa102f0c0a
alanmts-am <alanmts.am@gmail.com>
2024-06-27 21:28:45 -0300

    feat: add .env support

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 from app.routers.pais_router import paises_router
 from app.routers.estado_router import estado_router
 from app.routers.regiao_router import regiao_router
@@ -5,6 +6,9 @@ from app.routers.municipio_router import municipio_router
 
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 async def http_not_found(request, exc):
@@ -14,8 +18,11 @@ exception_handlers = {
     404: http_not_found
 }
 
-app = FastAPI(exception_handlers=exception_handlers)
-app.mount(path="/api", app=app)
+app = FastAPI(title="IBGE API",
+              description="This a IBGE api service to get data from countries, states, regions and cities",
+              version=os.getenv("API_VERSION"),
+              exception_handlers=exception_handlers)
+app.mount(path=os.getenv("API_BASE_PATH"), app=app)
 
 app.include_router(paises_router, prefix='/paises', tags=['paises'])
 app.include_router(estado_router, prefix='/estados', tags=['estados'])
